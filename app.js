2b39e945c04c3b24f4099925ebeaf957501d7ca7
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-05-05 14:26:47 -0700

    some HTML and DOM

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-// import functions and grab DOM elements
+import { renderElf } from './elf-utils.js';
 
-// let state
+const defeatedNumberEl = document.getElementById('defeated-number');
+const adventurerHp = document.getElementById('adventurer-hp');
+const adventurerImgEl = document.getElementById('adventurer-img');
+const form = document.querySelector('form');
+const elfListEl = document.querySelector('.elves');
+
+let defeatedElvesCount = 0;
+let playerHp = 100;
+let elves = [
+    { name: 'Legolas', hp: 30 },
+    { name: 'Galadriel', hp: 80 },
+];
 
 // set event listeners 
   // get user input
